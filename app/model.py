202c1a7c56b5aaa264fad429decd13ba3e96916d
bionca22 <bibicoder@gmail.com>
2023-08-03 20:56:11 -0300

    remove commit

--- app/model.py
+++ app/model.py
@@ -15,14 +15,3 @@ def get_user_option(message):
             return color_options[user_input]
         else:
             raise ValueError("Opção inválida. Tente novamente.")
-
-
-""" def color_validator(trigger, color):
-    try:
-        if color == "":
-            print("seu QRcode terá uma cor padrão então!")
-            color_bars = default_color_bars
-        else:
-            color_bars = color_validator(color_bars)
-    except ValueError:
-"""
